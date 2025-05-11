a6a81422b602b38b85d58d10ecf40b11d662e5db
Artem Kabritskii <artemkabr7@gmail.com>
2025-05-11 18:58:00 +0300

    доработка

--- src/handlers/navigation.py
+++ src/handlers/navigation.py
@@ -2,9 +2,9 @@
 
 from aiogram import types
 from aiogram.fsm.context import FSMContext
+from loader import dp
 from src.states.registration import RegistrationState
 from src.keyboards.inline import get_role_keyboard, get_main_menu
 
 @dp.callback_query(lambda c: c.data.startswith("back_"))
 async def handle_back(callback: types.CallbackQuery, state: FSMContext):
-    # ... код обработки навигации ...
